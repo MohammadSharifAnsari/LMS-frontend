9839cebf4232788a5e2a9bbe1a15b3376d338e28
MohammadSharifAnsari <mohammadsharifansari157@gmail.com>
2023-10-01 19:14:31 +0530

    axios setup

---
+++ src/helper/axiosinstance.js
@@ -0,0 +1,15 @@
+
+import axios from axios;
+
+// server ka url hai base url
+const BASE_URL='http:// localhost:7000';
+// const BASE_URL='http:// localhost:5014/api/v1';
+
+// axois.create() create the instance of axios
+const axiosInstance=axios.create();
+
+axiosInstance.defaults.baseURL=BASE_URL;
+axiosInstance.defaults.withCredentials=true;
+
+// you can read axios.intance
+
