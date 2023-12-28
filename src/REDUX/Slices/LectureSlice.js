97db618ffcfb3ba5f0d17087f4571fecaaa98bb1
MohammadSharifAnsari <mohammadsharifansari157@gmail.com>
2023-12-28 11:12:36 +0530

    added lecture slice

---
+++ src/REDUX/Slices/LectureSlice.js
@@ -0,0 +1,101 @@
+
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+import axiosInstance from "../../helper/axiosinstance";
+const initialState={
+    lectures:[]
+}
+
+
+//cid->it is an id of course
+export const  getCourseLectures=createAsyncThunk("/course/lecture/get",async (cid)=>{
+
+    try{
+const response=axiosInstance.get(`/course/${cid}`);
+
+toast.promise(response,{
+    loading:"Fetching course lectures",
+    success:"Lecture fetched successfully",
+    error:"Failed to load lecture"
+})
+
+return (await response).data;
+
+    }
+    catch(error){
+toast.error(error?.response?.data?.message);
+    }
+
+})
+export const  addCourseLectures=createAsyncThunk("/course/lecture/add",async (data)=>{
+
+    try{
+
+        const formData=new FormData();//yahan file ja rhi hai thats why we use formdata
+           formData.append("lecture",data.lecture);
+           formData.append("title",data.title);
+           formData.append("description",data.description);
+
+
+const response=axiosInstance.post(`/course/${data.id}`,formData);
+
+toast.promise(response,{
+    loading:"Adding course lectures",
+    success:"Lecture added successfully",
+    error:"Failed to add lecture"
+})
+
+return (await response).data;
+
+    }
+    catch(error){
+toast.error(error?.response?.data?.message);
+    }
+
+})
+export const  deleteCourseLectures=createAsyncThunk("/course/lecture/delet",async (data)=>{
+
+    try{
+
+const response=axiosInstance.delete(`/course?courseId=${data.courseId}&lectureId=${data.lectureId}`);
+
+toast.promise(response,{
+    loading:"Deleting course lecture",
+    success:"Lecture Deleted successfully",
+    error:"Failed to delete lecture"
+})
+
+return (await response).data;
+
+    }
+    catch(error){
+toast.error(error?.response?.data?.message);
+    }
+
+})
+
+
+
+const lectureSlice=createSlice({
+name:'lecture',
+initialState,               
+reducers:{},
+extraReducers:(builder)=>{
+
+builder.addCase(getCourseLectures.fulfilled,(state,action)=>{
+console.log(action);
+    state.lectures=action?.payload?.lecture;
+
+})
+.addCase(addCourseLectures.fulfilled,(state,action)=>{
+    console.log(action);
+
+    state.lectures=action?.payload?.course?.lectures;
+   
+})
+
+}                 
+               
+})
+
+export default lectureSlice.reducer;
--- src/REDUX/store.js
+++ src/REDUX/store.js
@@ -4,13 +4,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import authSliceReducer from "./Slices/authslice.js"
 import courseSliceReducer from "./Slices/Courseslice.js"
 import RazorPaySliceReducer from './Slices/RazorPaySlice.js'
+import lectureSliceReducer from './Slices/LectureSlice.js'
 
 const store=configureStore({
     reducer:{
 
         auth:authSliceReducer,
         course:courseSliceReducer,
-        razorpay:RazorPaySliceReducer
+        razorpay:RazorPaySliceReducer,
+        lecture:lectureSliceReducer
 
     },
     // jab hum deploy karenge tab alag tarah semanage karenge devtools ko
